4ed8dc4f5de1759b758dc11db64adc50a0508863
Daniel Belohlavek <138621639+rgbdani@users.noreply.github.com>
2026-07-11 16:43:24 -0300

    bun and node examples

--- examples/bun/src/main.ts
+++ examples/bun/src/main.ts
@@ -1,4 +1,3 @@
-import "fake-indexeddb/auto";
 import { TinyFS, O } from "@bielok/tinyfs";
 
 const tfs = await TinyFS.create("my-db");
@@ -8,7 +7,12 @@ await tfs.write(fd, new TextEncoder().encode("hello"), 5);
 await tfs.lseek(fd, 0, O.SET);
 
 const buf = new Uint8Array(5);
-await tfs.read(fd, buf, 5);
+const nr = await tfs.read(fd, buf, 5);
+console.log(new TextDecoder().decode(buf.subarray(0, nr)));
+
+const sb = { size: 0, mode: 0, nlink: 0 };
+await tfs.stat("/hello", sb);
+console.log("is dir?", (sb.mode & O.TYPE_MASK) === O.TYPE_DIR);
 
 tfs.close(fd);
 tfs.shutdown();
--- examples/node/src/main.ts
+++ examples/node/src/main.ts
@@ -1,4 +1,3 @@
-import "fake-indexeddb/auto";
 import { TinyFS, O } from "@bielok/tinyfs";
 
 const tfs = await TinyFS.create("my-db");
@@ -9,10 +8,11 @@ await tfs.lseek(fd, 0, O.SET);
 
 const buf = new Uint8Array(5);
 const nr = await tfs.read(fd, buf, 5);
+console.log(new TextDecoder().decode(buf.subarray(0, nr)));
 
 const sb = { size: 0, mode: 0, nlink: 0 };
 await tfs.stat("/hello", sb);
-const isDir = (sb.mode & O.TYPE_MASK) === O.TYPE_DIR;
+console.log("is dir?", (sb.mode & O.TYPE_MASK) === O.TYPE_DIR);
 
 tfs.close(fd);
 tfs.shutdown();
